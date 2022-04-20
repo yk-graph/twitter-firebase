2a1c622d9d5e9add1a01ce862783a1ec72884409
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-20 15:15:02 +0900

    bugfix: firebase authdomain修正

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,7 +6,7 @@ import 'firebase/storage'
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
