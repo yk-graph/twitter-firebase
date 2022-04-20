cfe01e71d0861d4e3934716ac4dc8b6a33a41b2c
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-20 11:47:52 +0900

    feat: firebase setting ver8

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// ver 8 での記述
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+export const db = firebaseApp.firestore()
+export const auth = firebaseApp.auth()
+export const storage = firebase.storage()
+
+// firebaseの外部認証機能（今回はGoogle認証）を使用するために必要なProvider
+export const provider = new firebase.auth.GoogleAuthProvider()
