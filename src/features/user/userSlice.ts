664555ffe86d14167bffe7c09c5f7b9857ba8e5a
TatsuyaYokokura <yokokura.tatsuya@gmail.com>
2022-04-27 10:08:36 +0900

    reset slice

---
+++ src/features/user/userSlice.ts
@@ -0,0 +1,31 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    user: { uid: "", photoUrl: "", displayName: "" },
+  },
+  reducers: {
+    /**
+     *@description ログイン認証が通ったユーザーの情報をグローバルステートに格納する関数
+     *@payload {uid: string, photoUrl: string, displayName: string}
+     */
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    /**
+     *@description ログアウト処理が通った場合、グローバルステートを初期化する関数
+     *@payload {uid: string, photoUrl: string, displayName: string}
+     */
+    logout: (state, action) => {
+      state.user = { uid: "", photoUrl: "", displayName: "" };
+    },
+  },
+});
+
+export const { login, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState) => state.user;
+
+export default userSlice.reducer;
